91b1682df5eac0c4e458960d64c572bd42cc6090
Killian <74175986+killianrochet@users.noreply.github.com>
2023-02-20 16:10:37 +0100

    Here is an example of code, creation of a template, then modification of the title for example. (The goal would be to modify the metadata but it is more readable with the title).
    This shows me an error, whether or not I put the body argument, or other.
    
    By going into the details of the patch_experiment_template() function, I could see that this function only takes id as argument. Does the error come from the ? Or is it me who misunderstood the use of the function?

--- examples/07.py
+++ examples/07.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
-import time
+
+
 import elabapi_python
 from elabapi_python.rest import ApiException
 
-# replace with your api key
-my_api_key = 'XXXX'
+#replace with your api key
+my_api_key = 'xxxxx'
 
 # Config
 configuration = elabapi_python.Configuration()
 configuration.api_key['api_key'] = my_api_key
 configuration.api_key_prefix['api_key'] = 'Authorization'
-configuration.host = 'XXXX'
+configuration.host = 'xxxx'
 configuration.debug = True
 configuration.verify_ssl = False
 
@@ -25,27 +26,31 @@ templateApi = elabapi_python.ExperimentsTemplatesApi(api_client)
 
 #Template that we want to create
 template_format = {
-    'body': '',
-    'canread': '{"base": 30, "teams": [], "users": [], "teamgroups": []}',
-    'canwrite': '{"base": 20, "teams": [], "users": [], "teamgroups": []}',
-    'fullname': 'User 1',
-    'is_pinned': 0,
-    'locked': 0,
-    'lockedby': None,
-    'lockedwhen': None,
-    'metadata': '{"extra_fields": {"End date": {"type": "date", "value": '
-                '"2021-06-09"}, "Ethical": {"type": "select", "value": '
-                '"123456", "options": ["123456", "20000", "12345"]}, '
-                '"Magnification": {"type": "select", "value": "20X", "options": '
-                '["10X", "20X", "40X"]}}}',
-    'tags': None,
-    'tags_id': None,
-    'teams_id': 21,
-    'title': 'Test API 3',
-    'userid': 6
+     'body': '',
+     'canread': '{"base": 30, "teams": [], "users": [], "teamgroups": []}',
+     'canwrite': '{"base": 20, "teams": [], "users": [], "teamgroups": []}',
+     'fullname': 'User 1',
+     'is_pinned': 0,
+     'locked': 0,
+     'lockedby': None,
+     'lockedwhen': None,
+     'metadata': '{"extra_fields": {"End date": {"type": "date", "value": '
+                 '"2021-06-09"}, "Ethical": {"type": "select", "value": '
+                 '"123456", "options": ["123456", "20000", "12345"]}, '
+                 '"Magnification": {"type": "select", "value": "20X", "options": '
+                 '["10X", "20X", "40X"]}}}',
+     'tags': None,
+     'tags_id': None,
+     'teams_id': 21,
+     'title': 'Test PATCH',
+     'userid': 6
 }
 
-#Use of the post funtion to create a new template with a body
-print(f'{templateApi.post_experiment_template(body=template_format)}')
+#Use of the post funtion to create a new template
+res = templateApi.post_experiment_template_with_http_info(body={'title': 'API Creation'})
+locationHeaderInResponse = res[2].get('Location')
+print(f'The newly created template is here: {locationHeaderInResponse}')
+itemId = int(locationHeaderInResponse.split('/').pop())
 
-
+#Change informations in the template
+templateApi.patch_experiment_template(itemId, body={'title': 'Test'})
