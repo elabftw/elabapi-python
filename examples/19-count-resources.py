77e84678f6b750adb1603e228e7105228cc1c107
Hüseyin Uzun <109156218+ELN-uzun@users.noreply.github.com>
2025-04-16 16:46:54 +0200

    Update 19-count-resources.py
    I hope I have correctly understood your comments =)

--- examples/19-count-resources.py
+++ examples/19-count-resources.py
@@ -1,35 +1,11 @@
 """
 This script connects to an eLabFTW instance via the REST API to retrieve and list all available resource categories (also called item types).
 For each category, it prints the category ID and title, followed by the number of entries (items) that belong to that category.
-
-⚙️ Configuration required:
-- API_HOST_URL: The URL of your eLabFTW instance.
-- API_KEY: Your personal or application-specific API key to authenticate requests.
-
-The script uses the official eLabFTW Python client and urllib3 to handle HTTPS connections.
 """
 
-# Import the necessary library to access the eLabFTW API
 import elabapi_python
-import urllib3
-
-API_HOST_URL = 'https://elab.local:3148/api/v2'  # <-- URL eLabFTW Goethe Uni or Replace with your instance URL 
-API_KEY = 'apiKey4Test'  # <-- Replace with your API key
-
-# Deactivate warnings for self-signed certificates (optional)
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-# Configure the API client
-configuration = elabapi_python.Configuration()
-configuration.api_key['api_key'] = API_KEY  
-configuration.api_key_prefix['api_key'] = 'Authorization'  
-configuration.host = API_HOST_URL  
-configuration.debug = False  
-configuration.verify_ssl = True  # Verify SSL certificate (important for secure communication)
-
-# Create the API client with the above configuration
-api_client = elabapi_python.ApiClient(configuration)
-api_client.set_default_header(header_name='Authorization', header_value=API_KEY)  
+# use the locally defined client.py module to get the api_client object, fully configured and ready to be used to instantiate api objects
+from client import api_client
 
 # Create API instances for fetching categories (ItemsTypes) and items (Entries) for each category
 items_api = elabapi_python.ItemsTypesApi(api_client)
