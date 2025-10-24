94dae5f342991bad726580d55aec3daafa4b5c15
MoustaphaCamara <cmr.mous@gmail.com>
2025-10-24 19:32:34 +0200

    updates & fix script 19

--- examples/14-fix-user-exp-permissions.py
+++ examples/14-fix-user-exp-permissions.py
@@ -11,32 +11,8 @@ import json
 
 # the python library for elabftw
 import elabapi_python
+from client import api_client
 
-#########################
-#         CONFIG        #
-#########################
-# replace with the URL of your instance
-API_HOST_URL = 'https://elab.local:3148/api/v2'
-# replace with your api key
-API_KEY = 'apiKey4Test'
-#########################
-#      END CONFIG       #
-#########################
-
-# Configure the api client
-configuration = elabapi_python.Configuration()
-configuration.api_key['api_key'] = API_KEY
-configuration.api_key_prefix['api_key'] = 'Authorization'
-configuration.host = API_HOST_URL
-configuration.debug = False
-configuration.verify_ssl = False
-
-# create an instance of the API class
-api_client = elabapi_python.ApiClient(configuration)
-# fix issue with Authorization header not being properly set by the generated lib
-api_client.set_default_header(header_name='Authorization', header_value=API_KEY)
-
-#### SCRIPT START ##################
 # Load the experiments api
 experimentsApi = elabapi_python.ExperimentsApi(api_client)
 
